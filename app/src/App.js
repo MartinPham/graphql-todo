424532c903bfddc0a2465c9b75643fdece88bb80
Martin Pham <tuyentq2009@gmail.com>
2019-08-09 17:34:10 +0200

    re organzie

--- app/src/App.js
+++ app/src/App.js
@@ -1,54 +1,11 @@
 import React from 'react';
 
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from '@apollo/react-hooks';
-
 import TaskList from './TaskList';
 import CreateTask from './CreateTask';
 
-import { split } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
-
-// Create an http link:
-const httpLink = new HttpLink({
-  uri: 'http://localhost:8081/graphql'
-});
-
-// Create a WebSocket link:
-const wsLink = new WebSocketLink({
-  uri: `ws://localhost:8081/graphql`,
-  options: {
-    reconnect: true
-  }
-});
-
-// using the ability to split links, you can send data to each link
-// depending on what kind of operation is being sent
-const link = split(
-  // split based on operation type
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink,
-);
-
-
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache()
-});
 
 function App() {
   return (
-    <ApolloProvider client={client}>
       <div>
         <h1>Hey</h1>
         <hr/>
@@ -56,7 +13,6 @@ function App() {
         <hr/>
         <TaskList/>
       </div>
-    </ApolloProvider>
   );
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,59 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8081/graphql'
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8081/graphql`,
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+
+ReactDOM.render(
+  (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  ), 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
